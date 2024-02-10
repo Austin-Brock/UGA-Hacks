20f04cf3dac05bcf8a14532550f1fcd1d0a6234e
Austin-Brock <135121147+Austin-Brock@users.noreply.github.com>
2024-02-10 02:49:40 -0500

    added 2 other sections for the pie chart

--- spending.js
+++ spending.js
@@ -2,14 +2,16 @@ document.addEventListener('DOMContentLoaded', function () {
     var ctx = document.getElementById('pieChart').getContext('2d');
     
     var data = {
-      labels: ['Salary', 'Investments', 'Other'],
+      labels: ['Rent', 'Food', 'Clothing', 'Investments', 'Other'],
       datasets: [{
         label: 'Income Distribution',
-        data: [50000, 20000, 10000], // Example data, replace with your actual income distribution
+        data: [50000, 20000, 10000, 12000, 15000], // Example data, replace with your actual income distribution
         backgroundColor: [
           'rgba(255, 99, 132, 0.6)',
           'rgba(54, 162, 235, 0.6)',
-          'rgba(255, 206, 86, 0.6)'
+          'rgba(255, 206, 86, 0.6)',
+          'rgba(75, 192, 192, 0.6)',
+        'rgba(153, 102, 255, 0.6)' ,
         ]
       }]
     };
