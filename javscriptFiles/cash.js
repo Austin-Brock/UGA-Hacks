579298a5b2a2db15bc910dc43cb5b347536ab9d3
AustinBrockUGA <135121147+Austin-Brock@users.noreply.github.com>
2024-02-10 13:28:33 -0500

    added calculating weekly to function

--- javscriptFiles/cash.js
+++ javscriptFiles/cash.js
@@ -2,5 +2,8 @@ function calculateMonthly() {
     const yearlySalary = document.getElementById('yearlySalary').value;
     const monthlyIncome = yearlySalary / 12;
     document.getElementById('monthlyIncome').innerText = monthlyIncome.toFixed(2);
+
+    const weeklyIncome = yearlySalary / 52;
+    document.getElementById('weeklyIncome').innerText = weeklyIncome.toFixed(2);
   }
   
